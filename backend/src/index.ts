9cfb9eeba36fa25b4a72e2017b0feade58fe94ef
hoang2807 <viethoang28072000@gmail.com>
2024-06-18 09:43:32 +0700

    feat: api repos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,33 @@
 import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
 
+import * as data from './data.json'
+
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server')
+interface ApiResponse {
+  fork: boolean
+  forks: number
+}
+
+app.get('/repos', async (req: Request, res: Response) => {
+  try {
+    // Rate limit
+    // const response = await fetch('https://api.github.com/users/freeCodeCamp/repos')
+
+    //  if (!response.ok) {
+    //   throw new Error(`API request failed with status: ${response.status}`);
+    // }
+
+    // const data = await response.json()
+    const filter = data.data.filter((f: ApiResponse) => f.fork === false && f.forks > 5)
+    res.status(200).json({ repositories: filter })
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 app.listen(port, () => {
