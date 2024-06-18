7f4495a1e99fa7cff139f06dcce2b80a417a148c
hoang2807 <viethoang28072000@gmail.com>
2024-06-18 08:55:24 +0700

    init commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import './reset.scss';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="container">
+        <h1>home</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
